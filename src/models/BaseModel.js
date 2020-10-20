486dc3e6ae99247d47b179509a4dbc1b533699b2
Charlie Ontiveros <dev@charlietheprogrammer.com>
2020-10-20 16:15:02 -0700

    My attempt at abstracing away the tedious Firebase realtime API and replacing it with a more user friendly API based on MongoDB. So far, seems to be working well.

---
+++ src/models/BaseModel.js
@@ -0,0 +1,145 @@
+import { db } from '../providers/Fire';
+import map from 'lodash/map';
+
+export class BaseModel {
+  constructor(modelName) {
+    this.name = 'recipes';
+  }
+
+
+  getKey() {
+    return this['.key'];
+  }
+
+  async save() {
+    if (this.getKey()) {
+      return this.constructor.updateOne(this);
+    } else {
+      return this.constructor.insertOne(this);
+    }
+	}
+
+	async delete() {
+		return this.constructor.deleteOne(this);
+  }
+  
+
+  static turnAllIntoModels(objArr) {
+    return map(objArr, (element, index) => {
+      let modelInstance = new this(element);
+      return Object.defineProperty(modelInstance, '.key', {
+				value: index,
+				enumerable: false,
+			});
+    });
+  }
+
+
+
+	/**
+	 * Returns a count of the number of documents in a collection
+	 */
+	static async count() {
+		try {
+			const modelName = this.name.toLowerCase();
+		} catch (error) {
+			console.log('Error occured while performing operation', error);
+		}
+	}
+
+	/**
+	 * Deletes one document from collection
+	 */
+	static async deleteOne(doc) {
+		try {
+      const modelName = this.name.toLowerCase();
+      return db.ref(modelName + '/' + doc['.key']).remove();
+		} catch (error) {
+			console.log('Error occured while performing operation', error);
+		}
+	}
+
+	/**
+	 * Find documents from collection
+	 * @param query object
+	 */
+	static async find(key = null, value = null) {
+		try {
+			const modelName = this.name.toLowerCase();
+			if (key && value) {
+				var snapshot = await db
+					.ref(modelName)
+					.orderByChild(key)
+					.equalTo(value)
+					.once('value');
+			} else {
+				var snapshot = await db.ref(modelName).once('value');
+			}
+			return this.turnAllIntoModels(snapshot.toJSON());
+		} catch (error) {
+			console.log('Error occured while performing operation', error);
+		}
+	}
+
+	/**
+	 * Finds one document from collection
+	 */
+	static async findOne() {
+		try {
+			const modelName = this.name.toLowerCase();
+		} catch (error) {
+			console.log('Error occured while performing operation', error);
+		}
+	}
+
+	/**
+	 * Inserts one or many documents into a collection
+	 * @param docs Array
+	 */
+	static async insert(docs) {
+		try {
+			const modelName = this.name.toLowerCase();
+			let promises = docs.map((item) => db.ref(modelName).push(docs));
+			return Promise.all(promises);
+		} catch (error) {
+			console.log('Error occured while performing operation', error);
+		}
+	}
+
+	/**
+	 * Inserts one document in a collection
+	 */
+	static async insertOne(doc) {
+		try {
+      debugger;
+			const modelName = this.name.toLowerCase();
+			return db.ref(modelName).push(doc);
+		} catch (error) {
+			console.log('Error occured while performing operation', error);
+		}
+	}
+
+	/**
+	 * Updates one or many documents in a collection
+	 */
+	static async update(doc) {
+		try {
+			const modelName = this.name.toLowerCase();
+		} catch (error) {
+			console.log('Error occured while performing operation', error);
+		}
+	}
+
+	/**
+	 * Updates one document in a collection
+	 */
+	static async updateOne(doc) {
+		try {
+			if (!doc['.key']) throw Error('Cannot update due to missing key.', doc);
+			const modelName = this.name.toLowerCase();
+			return db.ref(modelName + '/' + doc['.key']).set(doc);
+		} catch (error) {
+			console.log('Error occured while performing operation', error);
+		}
+	}
+}
--- src/models/Recipe.js
+++ src/models/Recipe.js
@@ -1,46 +1,14 @@
 import { db } from '../providers/Fire';
 
-export class Recipe {
-	genre = null;
+import { BaseModel } from './BaseModel';
 
-	constructor(name, user_id = null) {
+export class Recipe extends BaseModel {
+	// genre;
+
+	constructor({ name = arguments[0], user_id = arguments[1] }) {
+		super();
+
 		this.name = name;
-		this.user_id = user_id || window.user.id || null;
-		console.log(this);
-	}
-
-	save() {}
-
-	static async create(name = null, mealApiDTO = null) {
-		let recipe = null;
-		if (name) recipe = new Recipe(name);
-		// Get name from mealApi Object
-		else
-			recipe = new Recipe({
-				name: mealApiDTO.strMeal,
-			});
-		return db.ref('recipes').push(recipe);
-	}
-
-	static async delete(recipe) {
-		return db.ref('recipes/' + recipe['.key']).remove();
-	}
-
-	static async getAll() {
-		try {
-			const uid = window.user.id;
-			const ref = await db.ref('recipes');
-
-			// Need to implement the user id in the relationship
-			// const snapshot = await ref.equalTo(uid, 'uid').once('value');
-			const snapshot = await ref.once('value');
-
-			console.log(snapshot.toJSON());
-			
-			return snapshot.toJSON();
-
-		} catch (error) {
-			console.log('Error occured whuile fetching.', error);
-		}
+		this.user_id = user_id || window.user.id;
 	}
 }
