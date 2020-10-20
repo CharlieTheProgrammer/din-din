2ddbaa58dd3a3e7382dd3a25fca15491c968396b
Charlie Ontiveros <dev@charlietheprogrammer.com>
2020-10-19 20:15:41 -0700

    Updated recipe model. Still needs quite a bit of work, but it centralizes the logic for certain calls, which makes creating and fetching in the components a lot easier.

--- src/models/Recipe.js
+++ src/models/Recipe.js
@@ -2,18 +2,45 @@ import { db } from '../providers/Fire';
 
 export class Recipe {
 	genre = null;
-	
-	constructor(name) {
+
+	constructor(name, user_id = null) {
 		this.name = name;
-		
-		// When we create a new recipe, we should also save it?
+		this.user_id = user_id || window.user.id || null;
+		console.log(this);
 	}
 
-	static create(name) {
-		// need to wire up DB functions here
-		let recipe = new Recipe(name);
-		db.collection('recipes').add()
+	save() {}
+
+	static async create(name = null, mealApiDTO = null) {
+		let recipe = null;
+		if (name) recipe = new Recipe(name);
+		// Get name from mealApi Object
+		else
+			recipe = new Recipe({
+				name: mealApiDTO.strMeal,
+			});
+		return db.ref('recipes').push(recipe);
 	}
 
+	static async delete(recipe) {
+		return db.ref('recipes/' + recipe['.key']).remove();
+	}
 
+	static async getAll() {
+		try {
+			const uid = window.user.id;
+			const ref = await db.ref('recipes');
+
+			// Need to implement the user id in the relationship
+			// const snapshot = await ref.equalTo(uid, 'uid').once('value');
+			const snapshot = await ref.once('value');
+
+			console.log(snapshot.toJSON());
+			
+			return snapshot.toJSON();
+
+		} catch (error) {
+			console.log('Error occured whuile fetching.', error);
+		}
+	}
 }
