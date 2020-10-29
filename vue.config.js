c4d56a2f015fc57138bf0f96b3ee452ba1f79ffe
Charlie Ontiveros <dev@charlietheprogrammer.com>
2020-10-29 10:18:28 -0700

    Updated scripts and fixed a bug that caused queries to not work when compiling for production due to Webpack settings.

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,19 @@
+const TerserPlugin = require("terser-webpack-plugin");
+
 module.exports = {
   lintOnSave: false,
   devServer: {
     // I forgot why I turned this one, but if I turn it on Firebase Auth
     // no longer works due to invalid SSL cert. ugh...
-    https:false
+    https: false
+  },
+  chainWebpack: config => {
+    config.optimization.minimizer("terser").tap(args => {
+      const { terserOptions } = args[0]
+      // This is needed because base model class determines the model name at runtime and it must match the db collection name.
+      // Which won't match if it's mangled.
+      terserOptions.keep_fnames = true
+      return args
+    })
   }
 };
