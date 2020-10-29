987ba8c4edd6969d0297c948f98aa55d7a7ff416
Charlie Ontiveros <dev@charlietheprogrammer.com>
2020-10-29 13:12:54 -0700

    Formatting and bug fix. Recipe object is already included in route.params, so there's no need to have that as a separate object.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,94 +1,93 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
+import Vue from "vue";
+import VueRouter from "vue-router";
 
 // Import 'pages'
-import Login from '../views/Login';
-import Register from '../views/Register';
-import Landing from '../views/Landing';
-import Dashboard from '../views/Dashboard';
-import Recipes from '../views/Recipes';
-import Recipe from '../views/Recipe';
-import CreateRecipe from '../views/CreateRecipe';
-import MenuOfTheWeek from '../views/components/MenuOfTheWeek';
+import Login from "../views/Login";
+import Register from "../views/Register";
+import Landing from "../views/Landing";
+import Dashboard from "../views/Dashboard";
+import Recipes from "../views/Recipes";
+import Recipe from "../views/Recipe";
+import CreateRecipe from "../views/CreateRecipe";
+import MenuOfTheWeek from "../views/components/MenuOfTheWeek";
 
 // Other imports
-import { firebase } from '../providers/Fire';
+import { firebase } from "../providers/Fire";
 
 Vue.use(VueRouter);
 
 let router = new VueRouter({
-	mode: 'history',
-	routes: [
-		{
-			path: '/',
-			name: 'landing',
-			component: Landing,
-		},
-		{
-			path: '/login',
-			name: 'login',
-			component: Login,
-		},
-		{
-			path: '/register',
-			name: 'register',
-			component: Register,
-		},
-		{
-			path: '/dashboard',
-			name: 'dashboard',
-			component: Dashboard,
-			meta: {
-				requiresAuth: true,
-			},
-		},
-		{
-			path: '/recipe-create',
-			name: 'create-recipe',
-			component: CreateRecipe,
-			meta: {
-				requiresAuth: true,
-			},
-		},
-		{
-			path: '/recipes/:id',
-			name: 'recipe',
-			component: Recipe,
-			meta: {
-				requiresAuth: true,
-			},
-			props: (route) => ({
-				recipe: {},
-				...route.params
-			})
-		},
-		{
-			path: '/recipes',
-			name: 'recipes',
-			component: Recipes,
-			meta: {
-				requiresAuth: true,
-			}
-		},
-		{
-			path: '/weekly-menu',
-			name: 'weekly-menu',
-			component: MenuOfTheWeek,
-			meta: {
-				requiresAuth: true,
-			}
-		}
-	],
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      name: "landing",
+      component: Landing
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: Register
+    },
+    {
+      path: "/dashboard",
+      name: "dashboard",
+      component: Dashboard,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: "/recipe-create",
+      name: "create-recipe",
+      component: CreateRecipe,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: "/recipes/:id",
+      name: "recipe",
+      component: Recipe,
+      meta: {
+        requiresAuth: true
+      },
+      props: route => ({
+        ...route.params
+      })
+    },
+    {
+      path: "/recipes",
+      name: "recipes",
+      component: Recipes,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: "/weekly-menu",
+      name: "weekly-menu",
+      component: MenuOfTheWeek,
+      meta: {
+        requiresAuth: true
+      }
+    }
+  ]
 });
 
 // Nav Guards
 router.beforeEach(async (to, from, next) => {
-	const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
-	if (requiresAuth && !(await firebase.getCurrentUser())) {
-		next('login');
-	} else {
-		next();
-	}
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+  if (requiresAuth && !(await firebase.getCurrentUser())) {
+    next("login");
+  } else {
+    next();
+  }
 });
 
 export default router;
